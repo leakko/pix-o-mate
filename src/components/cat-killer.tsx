569cbc67e417fd501f05f27463fb331900c8a285
Marcos R <marcossalvo95@gmail.com>
2023-10-02 23:56:48 +0200

    Fix typo

--- src/components/cat-killer.tsx
+++ src/components/cat-killer.tsx
@@ -2,11 +2,11 @@ import { useState } from 'react';
 import { FaCat } from 'react-icons/fa';
 
 export const CatKiller = () => {
-	const [ catsKilled, setCatsKilled ] = useState<number>(0);
+	const [ killedCats, setKilledCats ] = useState<number>(0);
 	return (
 		<div className='flex items-center'>
 			<FaCat />
-			<p className='ml-1'>{ catsKilled }</p>
+			<p className='ml-1'>{ killedCats }</p>
 		</div>
 	)
 }
