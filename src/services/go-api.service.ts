6f146b6cd3dc68df0fec2b7add319a33b0f477d1
Marcos R <marcossalvo95@gmail.com>
2023-10-02 23:56:10 +0200

    Fix typo

--- src/services/go-api.service.ts
+++ src/services/go-api.service.ts
@@ -3,7 +3,7 @@ import { Owner } from '../types/owner';
 import { GO_API_ENDPOINTS } from '../data/constants/go-api-endpoints';
 
 class GoApiService {
-	getUsers(): Promise<{ data: User[] }> {
+	getUsers(): Promise<{ data: Owner[] }> {
 		return axios.get(GO_API_ENDPOINTS.USERS, {
 			headers: { Authorization: `Bearer ${import.meta.env.VITE_GO_REST_TOKEN}` }
 		})
